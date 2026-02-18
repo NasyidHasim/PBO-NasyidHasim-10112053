8f7bcf1b130c55b625bd24d615a23792e568f447
D'hasynid <nasyidbina@gmail.com>
2026-02-18 19:37:39 +0700

    Refactor produk class to use static bunga

--- Proses2.php
+++ Proses2.php
@@ -1,11 +1,11 @@
 <?php
 class produk {
     public $pinjaman;
-    public $bunga;
+    public static $bunga = 10;
     public $lama;
 
     public function Totalpinjaman(){
-        return $this->pinjaman + ($this->pinjaman / $this->bunga);
+        return $this->pinjaman + ($this->pinjaman / self::$bunga);
     }
     public function Angsuran(){
         return $this->Totalpinjaman() / $this->lama;  
@@ -13,14 +13,13 @@ class produk {
 }
 $produk1 = new produk();
 $produk1->pinjaman = htmlspecialchars($_POST['nama']);
-$produk1->bunga = htmlspecialchars($_POST['harga']);
 $produk1->lama = htmlspecialchars($_POST['lama']);
 
 
 echo "<h2>toko pegadaian syariah</h2>"."<br>";
 echo "____________________________________________________________"."<br>";
 echo "besar pinjaman :RP " . $produk1->pinjaman . "<br>";
-echo "besar bunga : " . $produk1->bunga . "%" . "<br>";
+echo "besar bunga : " . produk::$bunga . "%" . "<br>";
 echo "lama pinjaman : " . $produk1->lama . " bulan" . "<br>";
 echo "___________________________________________________________"."<br>";
 echo "Total pinjaman :RP " . $produk1->Totalpinjaman() . "<br>";
