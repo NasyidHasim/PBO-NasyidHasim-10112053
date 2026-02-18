655733da5e0d302686b00d5eed9fb1e26bf77f0d
D'hasynid <nasyidbina@gmail.com>
2026-02-18 19:39:20 +0700

    Update form-produk.php to include multiple forms
    Added a second form for products without late fees.

--- form-produk.php
+++ form-produk.php
@@ -3,25 +3,32 @@
     <title>Toko Pegadaian Syariah</title>
 </head>
 <body>
-    <h1>Toko pegadaian </h1>
+    <h2>Toko Pegadaian</h2>
     <h2>terimakasih telah menggunakan layanan kami</h2>
-    <h4>input data produk</h4>
+    <h4>input data produk dengan denda keterlambatan</h4>
     <form action="proses.php" method="post">
         besar pinjaman:
-        <input type="text" name="nama"> <br><br>
+        <input type="text" name="pinjaman"> <br>
         
-        besar bunga:
-        <input type="number" name="harga"> <br><br>
-
         lama pinjaman:
-        <input type="number" name="lama"> <br><br>
+        <input type="number" name="lama"> <br>
        
-        keterlambatan:
-        <input type="number" name="keterlambatan"> <br><br>
+        keterlambatan: 
+        <input type="number" name="keterlambatan"> <br>
 
         <input type="submit" value="Simpan">
 
     </form>
+    <h4>input data produk dengan tanpa denda </h4>
+    <form action="proses2.php" method="post">
+        besar pinjaman:
+        <input type="text" name="nama"> <br>
+
+        lama pinjaman:
+        <input type="number" name="lama"> <br>
     
+        <input type="submit" values="simpan">
+
+    </form>
 </body>
 </html>
