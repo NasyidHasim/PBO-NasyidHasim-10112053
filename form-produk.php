7c862be95b0c2b7bd242ba0cf323f0f33c9ac200
D'hasynid <nasyidbina@gmail.com>
2026-02-18 12:54:56 +0700

    Add files via upload

---
+++ form-produk.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+    <title>Toko Pegadaian Syariah</title>
+</head>
+<body>
+    <h1>Toko pegadaian </h1>
+    <h2>terimakasih telah menggunakan layanan kami</h2>
+    <h4>input data produk</h4>
+    <form action="proses.php" method="post">
+        besar pinjaman:
+        <input type="text" name="nama"> <br><br>
+        
+        besar bunga:
+        <input type="number" name="harga"> <br><br>
+
+        lama pinjaman:
+        <input type="number" name="lama"> <br><br>
+       
+        keterlambatan:
+        <input type="number" name="keterlambatan"> <br><br>
+
+        <input type="submit" value="Simpan">
+
+    </form>
+    
+</body>
+</html>
